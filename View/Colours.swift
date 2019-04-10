256b53baa54ef11dd7d0600068c2f4cec7572762
gladiusKatana <garthsnyder@protonmail.com>
2019-04-10 18:42:54 -0400

    Removed unused custom colours ⚔️ (yes as you may have guessed, I'm Canadian 🇨🇦(as seen by my strange spelling of 'colours'). When I'm in the US, though, I spell things the American way. Colors🇺🇸)

--- View/Colours.swift
+++ View/Colours.swift
@@ -16,28 +16,19 @@ var headerColour = UIColor.COOOKIE(NUM, g: NUM, b: NUM, a: 1) // https://www.you
 var cellDefaultColour = navyBlue
 
 
-var randomColours = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor, UIColor.white.cgColor]
 
-
-var grayPointZeroFive = UIColor.COOOKIE(0, g: 0, b: 0, a: 0.03)
-var grayOne = UIColor.COOOKIE(0, g: 0, b: 0, a: 0.1)
 var grayTwo = UIColor.COOOKIE(0, g: 0, b: 0, a: 0.2)
 var graySeven = UIColor.COOOKIE(0, g: 0, b: 0, a: 0.7)
 
-
 var platinum = UIColor.COOOKIE(214, g: 214, b: 214, a: 1)
-var platinumMedium = UIColor.COOOKIE(214, g: 214, b: 255, a: 0.8)
-var platinumLite = UIColor.COOOKIE(214, g: 214, b: 255, a: 0.45)
 var platinumUltraLite = UIColor.COOOKIE(214, g: 214, b: 255, a: 0.25)
-var platinumBarelyThere = UIColor.COOOKIE(214, g: 214, b: 255, a: 0.1)
-
 
 var icyBlue = UIColor.COOOKIE(150, g: 180, b: 220, a: 1)
-var darkerIcyBlue = UIColor.COOOKIE(100, g: 140, b: 200, a: 1)
 var navyBlue = UIColor.COOOKIE(0, g: 36, b: 82, a: 1)
-//(30, green: 40, blue: 160, alpha: 1) looked ok too
 
 
 var skyBackgroundImage = UIImage(named: "skyB")?.withRenderingMode(.alwaysOriginal)
-var skyBackgroundImageColour = UIColor(patternImage: skyBackgroundImage!)
+var skyBackgroundColour = UIColor(patternImage: skyBackgroundImage!)                 //may use
 
+
+var randomColours = [UIColor.red.cgColor, UIColor.orange.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor, UIColor.blue.cgColor, UIColor.purple.cgColor, UIColor.white.cgColor]                                   // used in TestSquare.swift

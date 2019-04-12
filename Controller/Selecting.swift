860b38c4a184f714e7f6e06636157b8cc688759e
gladiusKatana <garthsnyder@protonmail.com>
2019-04-12 13:07:34 -0400

    Added selection handler method to CollectionVC

---
+++ Controller/Selecting.swift
@@ -0,0 +1,14 @@
+//  Selecting.swift
+//  DMY-Tabs-Calendar  ∙  1st commit Apr. 07, 2019  ∙  Created by Garth Snyder a.k.a. gladiusKatana ⚔️
+
+import UIKit
+
+extension CollectionVC {
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! CustomCell
+        
+        
+    }
+
+}

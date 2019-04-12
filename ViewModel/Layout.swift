aebc9edda6d856551414abc9436858562518d67f
gladiusKatana <garthsnyder@protonmail.com>
2019-04-12 11:28:34 -0400

    Regrouped some files, with a new group "ViewModel" ⚔️ this is to follow the MVC pattern and one aspect of the MVVM pattern (the 'VM') in this program.  I have tended to use these patterns somewhat loosely, in general, asking essentially ''what seems to be the best category?'' for each code file. I have preferred to let the application decide which pattern(s) will emerge from the collection of files, classes, methods, etc., rather than deciding a priori whether to conform to MVC, or MVVM, etc. for organization.  The principal pattern I have always tried to follow, is the engineering design principle of modularity, while (also crucially) optimizing for fast navigation between modules in Xcode (shorter code files for minimal scrolling, maximal use of hotkeys and mouse navigation-buttons, etc.)  Fast navigation is the forcing function for more numerous code files, of course; but in my estimation, *classification* of said files when organizing them is somewhat subjective (for example, does a collection view layout belong under 'controller' or 'view model'? After all, it is a model of the view (how cells are displayed), but it gets passed into a view controller. Here is a little debate on that from others: https://stackoverflow.com/questions/21664166/where-does-a-collection-view-flow-layout-fit-in-mvc)

--- ViewModel/Layout.swift
+++ ViewModel/Layout.swift
@@ -5,9 +5,9 @@ import UIKit
 
 class CCVFlowLayout : UICollectionViewFlowLayout {  // stands for "Custom Collection View Flow Layout"
     var cellWidth: CGFloat?;        var cellHeight: CGFloat?
-    var hSpace = CGFloat(0);        var vSpace = CGFloat(0);    var cellWd = CGFloat(0);            var cellHt = CGFloat(0)
-    var xDefault = CGFloat(0);      var yDefault = CGFloat(0);  var widthPlusSpace = CGFloat(0);    var heightPlusSpace = CGFloat(0)
-    var cols = 0;                   var rows = 0;               var lockedHeaderRows = 0;           var lockedHeaderSections = 0
+    var hSpace = CGFloat(0);        var vSpace = CGFloat(0);        var cellWd = CGFloat(0);            var cellHt = CGFloat(0)
+    var xDefault = CGFloat(0);      var yDefault = CGFloat(0);      var widthPlusSpace = CGFloat(0);    var heightPlusSpace = CGFloat(0)
+    var cols = 0;                   var rows = 0;                   var lockedHeaderRows = 0;           var lockedHeaderSections = 0
     var contentSize = CGSize.zero
     var cellDimensionsMode = CellDimensionsMode.neitherHardcoded;   var loadsHorizontally = false
     var squareCellMode = SquareCellMode.noAutoSquare;               var squareCells = false
